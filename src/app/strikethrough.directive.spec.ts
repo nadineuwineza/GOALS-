60c7601b179acd2e68eba00599e4b743a13fe0e3
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-06 10:48:04 +0200

    initial commit

--- src/app/strikethrough.directive.spec.ts
+++ src/app/strikethrough.directive.spec.ts
@@ -1,4 +1,4 @@
-import { StrikethroughDirective } from './strikethrough.directive';
+import {StrikethroughDirective } from './strikethrough.directive';
 
 describe('StrikethroughDirective', () => {
   it('should create an instance', () => {
